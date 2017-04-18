075f418e3b3e7b428ac11a4b31efc2f4dc486b13
Long Hoang <long@mindworker.de>
2017-04-18 17:21:16 +0800

    Add IPFS init to setup

--- setup_node.sh
+++ setup_node.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-IP=$1
+mkdir data
+IPFS_PATH=./data/MyNode/ ipfs init
 
-ssh ubuntu@$IP 'git clone --depth 1 https://github.com/turbotardigrade/agora nettest; cd nettest; go build; ./install_curation.sh'
+# IP=$1
+# ssh ubuntu@$IP 'source .profile; git clone --depth 1 https://github.com/turbotardigrade/agora nettest; cd nettest; go build; ./install_curation.sh'

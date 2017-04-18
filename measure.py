8ba957f808b3c2641309ec6de00a8b19d4e9ebff
Long Hoang <long@mindworker.de>
2017-04-18 16:01:58 +0800

    Run X iterations of the test

--- measure.py
+++ measure.py
@@ -1,16 +1,20 @@
 from subprocess import Popen, PIPE, STDOUT
+from datetime import datetime
+
 import time
 import os
 import sys
 
-if len(sys.argv) != 2:
+if len(sys.argv) != 3:
     print 'Incorrect usage. Please follow this:'
-    print '\tpython measure.py [IP]'
+    print '\tpython measure.py [IP] [# of iterations]'
     sys.exit(1)
 
 IP = sys.argv[1]
-env = os.environ.copy()
-env["IPFS_PATH"] = "./data/MyNode"
+N = int(sys.argv[2])
+
+ENV = os.environ.copy()
+ENV["IPFS_PATH"] = "./data/MyNode"
 
 def reset_remote_node(IP):
     p = Popen(['ssh', 'ubuntu@'+IP], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
@@ -30,19 +34,22 @@ def reset_remote_node(IP):
         if 'Seed for' in line:
             return peer, close
 
-peer, close = reset_remote_node(IP)
-start, hasPong = time.time(), False
-while not hasPong:
-    pm = Popen(['ipfs', 'ping', peer], stdout=PIPE, stderr=STDOUT, env=env)
-    for line in iter(pm.stdout.readline, b''):
-        if 'Pong received' in line:
-            hasPong = True
-            elapsed = time.time()-start
-            ping = line.split(' ')[-2][5:]
-            break
-        pm.wait()
-close()
+for _ in xrange(N):
+    peer, close = reset_remote_node(IP)
+    start, hasPong = time.time(), False
+    while not hasPong:
+        pm = Popen(['ipfs', 'ping', peer], stdout=PIPE, stderr=STDOUT, env=ENV)
+        for line in iter(pm.stdout.readline, b''):
+            if 'Pong received' in line:
+                hasPong = True
+                elapsed = time.time()-start
+                ping = line.split(' ')[-2][5:]
+                break
+            pm.wait()
 
-print elapsed, ping
-with open("times.csv", "a") as f:
-    f.write("%f, %s\n" % (elapsed, ping))
+        # print 'No Pong. Restart...'
+    close()
+
+    print elapsed, ping
+    with open("times.csv", "a") as f:
+        f.write("%s, %f, %s\n" % (datetime.now().time(), elapsed, ping))
